26fbbf5fbe628caf29dfa8b0904f5c6d7c95c923
Shaun W <shaun.p.wassell@gmail.com>
2020-05-28 13:15:08 -0400

    01.11 End

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-listing-page.component.css']
 })
 export class NewListingPageComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  price: string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
+  }
 }
